1459a868a3054dd0eb7e0541c9f749f3e34b1297
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-11-11 10:53:50 -0500

    An image was created in UIKIt using code, certain characteristics such as size, color, border, among others, are modified.

--- UIKit5_UIImageViewUsingCode/ViewController.swift
+++ UIKit5_UIImageViewUsingCode/ViewController.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let swiftBetaImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "gamecontroller.fill")
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .purple
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.layer.cornerRadius = 150
+        imageView.layer.borderWidth = 10
+        imageView.layer.borderColor = UIColor.blue.cgColor
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(swiftBetaImageView)
+        
+        NSLayoutConstraint.activate([
+            swiftBetaImageView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            swiftBetaImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            swiftBetaImageView.widthAnchor.constraint(equalToConstant: 300),
+            swiftBetaImageView.heightAnchor.constraint(equalToConstant: 300),
+
+        ])
     }
 
 
